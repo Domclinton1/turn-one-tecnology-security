b8203bea3d06576aa3cdaaf8845bc4b116440185
Domclinton1 <clinton.admpro29@gmail.com>
2026-01-14 12:19:09 -0300

    melhorando hero

--- script.js
+++ script.js
@@ -99,3 +99,19 @@ nav.addEventListener("click", (e) => {
     closeMenu();
   }
 });
+
+const cards = document.querySelectorAll(".about-card");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = "1";
+        entry.target.style.transform = "translateY(0)";
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+cards.forEach((card) => observer.observe(card));
